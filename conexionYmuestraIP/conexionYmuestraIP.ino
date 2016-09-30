1006e55224c604482eecc8c2207cf921a2e05bd2
Trastejant <nano@trastejant.com>
2016-09-30 17:04:48 +0200

    Muestra la IP y el nombre de la red a la que estamos conectados

---
+++ conexionYmuestraIP/conexionYmuestraIP.ino
@@ -0,0 +1,73 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+
+
+SSD1306  display(0x3c, D1, D2);
+
+const char *ssid = "MOVISTARTL";
+const char *password = "Martin89";
+
+
+ESP8266WebServer server ( 80 );
+
+
+void setup ( void ) {
+ Serial.begin ( 115200 );
+ WiFi.begin ( ssid, password );
+ Serial.println ( "" );
+
+   display.init();
+ // Wait for connection
+ while ( WiFi.status() != WL_CONNECTED ) {
+  delay ( 500 );
+  Serial.print ( "." );
+    display.print(".");
+ }
+
+ Serial.println ( "" );
+ Serial.print ( "Connected to " );
+ Serial.println ( ssid );
+ Serial.print ( "IP address: " );
+ Serial.println ( WiFi.localIP() );
+
+ 
+ /*
+  String tempIP =WiFi.localIP().toString(); 
+  char testChar[20];
+  tempIP.toCharArray(testChar,20);
+  display.print(testChar);
+  */
+    display.clear();
+    
+ 
+ if ( MDNS.begin ( "esp8266" ) ) {
+  Serial.println ( "MDNS responder started" );
+ }
+
+  Serial.println ( "HTTP server started" );
+
+
+
+}
+
+void loop() {
+   
+ display.setTextAlignment(TEXT_ALIGN_LEFT);
+ display.setFont(ArialMT_Plain_24);
+    display.drawString(0, 0, "Jarvis");
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 30, "En:");
+    display.drawString(30, 30, ssid);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 46, "IP:");
+    display.drawString(15, 46, "192.168.1.43");
+    display.display();
+ 
+}
+
+
